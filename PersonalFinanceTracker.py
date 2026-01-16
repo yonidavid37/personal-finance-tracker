563cbf06f7be052b7c0ea374baa3c6f35fab1812
yonidavid37 <yonidavid37@gmail.com>
2026-01-16 12:47:55 +0200

    Fix Flask template path

--- PersonalFinanceTracker.py
+++ PersonalFinanceTracker.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 # ===== נתונים פיננסיים (כרגע סטטיים) =====
 finance_data = {
